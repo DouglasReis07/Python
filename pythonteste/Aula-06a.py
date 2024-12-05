b0cf5cef897b770190e4b06bd90822e702cba19f
DouglasReis07 <163474683+DouglasReis07@users.noreply.github.com>
2024-12-05 20:27:46 -0300

    Exercicos

---
+++ pythonteste/Aula-06a.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+s = n1 + n2
+#print('A soma vale entre', n1 , 'e5' , n2 , ' é ' , s )
+print ('A soma entre {} e {} vale {}'.format(n1,n2,s))
